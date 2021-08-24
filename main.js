fee3733f8291182a1c2292b2cb478e8f54fd8e96
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-08-23 18:44:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function startClassification() {
+navigator.mediaDevices.getUserMedia({ audio: true});
+classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/T3eGPh_Dq/model.json', modelReady);
+}
+
+function modelReady() {
+classifier.classify(gotResults);
+}
+
+function gotResults(error, results) {
+console.log(gotResults);
+}
